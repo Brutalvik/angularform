fb566a8487c8f4e819474ad94564603a0b638ece
Vikram Kumar <vikcanada90@gmail.com>
2021-04-26 17:54:25 -0400

    Changed Event Binding to 2 Way binding using ngModel Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppformComponent } from './appform/appform.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppformComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username = '';
+  password = '';
+  logintext = '';
+  clicked = false;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+/*  // tslint:disable-next-line:typedef
+  fetchUser(event: any){
+    this.username = event.target.value;
+    console.log(this.username);
+  }
+
+  // tslint:disable-next-line:typedef
+  fetchPassword(event: any){
+    this.password = event.target.password;
+    console.log(this.password);
+  }*/
+
+  // tslint:disable-next-line:typedef
+  onLogin() {
+    this.clicked = true;
+    this.logintext = 'You are now Logged In';
+    console.log(this.username, this.password)
+    this.username = '';
+    this.password = '';
+  }
+
+  // tslint:disable-next-line:typedef
+  onLogout() {
+    this.clicked = false;
+    this.logintext = 'You have now successfully Logged Out.';
+
   }
 
 }
